096bfd7815299afe23b9e1e4ca4ddce9c686b9d3
ebrahim20254 <ebrahimkhalil125800@gmail.com>
2023-09-01 15:33:54 +0600

    blog section done

--- index.js
+++ index.js
@@ -12,29 +12,32 @@ const handleCategory = async () => {
 
 // show data picture 
 const handleLoadData = async (categoryId) => {
-    // console.log(categoryId);
+    console.log(categoryId);
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await res.json();
     console.log(data.data);
 
     const cardContainer  = document.getElementById('card-container');
+    cardContainer.innerHTML = "";
     data.data.forEach((hero)=>{
         // console.log(hero);
         const div = document.createElement('div');
-        div.innerHTML = `<div class="card space-y-8 bg-base-100 shadow-xl">
-        <figure><img  src="${hero?.thumbnail}" alt="Shoes" /></figure>
-        <div class="flex gap-4 ">
-         <img class="w-12" src="${hero?.authors[0].profile_picture}" alt="">
-          <p>${hero?.title}</p>
+        div.innerHTML = `<div class="card h-full space-y-4 bg-gray-200 shadow-xl">
+        <figure class = "w-full"><img  src="${hero?.thumbnail}" alt="Shoes" /></figure>
+          
+        <div class="flex items-center gap-4 ">
+         <img class="w-12 rounded-full" src="${hero?.authors[0].profile_picture}" alt="">
+          <p class="font-bold">${hero?.title}</p>
         </div>
-          <div class="flex gap-4">
+          <div class="flex gap-2 justify-center">
           <h3>${hero.authors[0].profile_name}</h3>
           <img src="./image/right.png" alt="">
         </div>
-        <p>${hero.others.views}</P>
+        <p class="text-center">${hero.others.views}</P>
       </div>`;
       cardContainer.appendChild(div);
     });
 
 }
 handleCategory();
+handleLoadData();

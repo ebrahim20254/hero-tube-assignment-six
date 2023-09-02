3dd5a1d03a9fd97b47c43edd65bccaa982ce64f6
ebrahim20254 <ebrahimkhalil125800@gmail.com>
2023-09-02 18:58:26 +0600

    card api create done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const handleCategory = async () => {
         heroDiv.innerHTML = ` <a onclick =  handleLoadData(${data.category_id}) class="tab">${data.category}</a> `;
         heroContainer.appendChild(heroDiv);
     });
+
     // console.log(data.data);
 }
 
@@ -18,26 +19,81 @@ const handleLoadData = async (categoryId) => {
     console.log(data.data);
 
     const cardContainer  = document.getElementById('card-container');
+    // error massage do data 
+        const noImage = document.getElementById('error-massage');
+        const errorDiv = document.createElement('div');
+        errorDiv.innerHTML=`  <div class="flex justify-center">
+          <img src="./image/Icon.png" alt="">
+         </div>
+         <p class="font-bold text-4xl text-center mt-4">Oops!! Sorry, There is no <br> content here</p>`;
+        if(data.length > 0){
+          errorDiv.classList.remove('hidden')
+        }else{
+          errorDiv.classList.add('hidden')
+        }
+        noImage.appendChild(errorDiv);
     cardContainer.innerHTML = "";
     data.data.forEach((hero)=>{
-        // console.log(hero);
+        // console.log(hero);  
         const div = document.createElement('div');
         div.innerHTML = `<div class="card h-full space-y-4 bg-gray-200 shadow-xl">
         <figure class = "w-full"><img  src="${hero?.thumbnail}" alt="Shoes" /></figure>
-          
         <div class="flex items-center gap-4 ">
-         <img class="w-12 rounded-full" src="${hero?.authors[0].profile_picture}" alt="">
+         <img class="h-12 w-12 rounded-full" src="${hero?.authors[0].profile_picture}" alt="">
           <p class="font-bold">${hero?.title}</p>
         </div>
-          <div class="flex gap-2 justify-center">
+          <div class="flex gap-2">
           <h3>${hero.authors[0].profile_name}</h3>
-          <img src="./image/right.png" alt="">
+          <img src="${hero?.authors[0]?.verified ? hero.authors[0].verified: '/image/right.png' }" alt="">
         </div>
-        <p class="text-center">${hero.others.views}</P>
+        <p>${hero.others.views}</P>
       </div>`;
       cardContainer.appendChild(div);
+         
     });
+        
 
-}
+};
+// const handleShortBtn = () => {
+//   // console.log('button clicked');
+//   const shortData = cardContainer.sort((a,b)=>{
+//     a = a?.others.views;
+//     a = parseFloat(a.replace("k", ""));
+
+//     b = b?.others.views;
+//     b = parseFloat(b.replace("k",""));
+//     return b-a;
+//   });
+//   cardContainer(sh)
+// }
+
+// function handleTimeSet(){
+//   console.log(second);
+
+//   const days = Math.floor(second / (3600 * 24));
+//   second -= days * (3600 * 24);
+
+//   const hours = Math.floor(second / 3600);
+//   second -= hours * 3600;
+
+//   const minutes =  Math.floor(second / 60);
+//   second -= minutes * 60;
+
+//   let handleTimeSet = "";
+
+//   if(days > 0){
+//     handleTimeSet += `${days} day`;
+//     if(days >1){
+//       handleTimeSet += "s";
+//     }
+//   }
+// }
+
+{/* <p onclick="handleTimeSet()" class="text-end">${hero.others.posted_date}</p> */}
+
+
+
+
+
 handleCategory();
 handleLoadData();
